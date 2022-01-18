9f4f0b6c046ff93b0968d147b580095132cbeab5
kennotabot <kendsilva@Kens-MacBook-Air.local>
2022-01-18 23:30:00 +0530

    added arrays and multi-domemnsional arrays concept

--- src/com/dsa/ArraysPractice.java
+++ src/com/dsa/ArraysPractice.java
@@ -1,5 +1,7 @@
 package com.dsa;
 
+import java.util.Arrays;
+
 public class ArraysPractice {
     public static void main(String[] args){
         int[] rollnos = new int[5];
@@ -9,7 +11,19 @@ public class ArraysPractice {
         dynamic memory allocation
 
         array object reference is stack memory
-        array object is stored in heap memory **/
+        array object is stored in heap memory
+
+        Not a contiguous memory allocation
+        heap allocation depends on JVM
+
+        if no value is allocated all indexes have
+        0 for integer array & null for string array **/
+
+        for (int i: rollnos2){
+            System.out.print(i + " ");
+        }
+
+        System.out.println(Arrays.toString(rollnos2));
 
     }
 }
---
+++ src/com/dsa/MultiDimensionArray.java
@@ -0,0 +1,11 @@
+package com.dsa;
+
+public class MultiDimensionArray {
+    public static void main(String[] args){
+
+        int[][] arr1 = new int[3][];
+        // columns need not be specified, no of row is mandatory
+        // because individual array can vary in size
+
+    }
+}
